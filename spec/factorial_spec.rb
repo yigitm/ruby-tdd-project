e4c220893de6d94ac257b9a449be621b86a0d790
Yigit Mersin <yigitmersin@gmail.com>
2022-03-09 22:53:33 +0300

    Add test for factorial method

---
+++ spec/factorial_spec.rb
@@ -0,0 +1,17 @@
+describe 'Solver Class' do
+  before :each do
+    @solver = Solver.new
+  end
+
+  it 'should return factorial of given number of 0' do
+    expect(@solver.factorial(0)).to eq 1
+  end
+
+  it 'should return factorial of given number of -1' do
+    expect(@solver.factorial(-1)).to raise_error('No negative integer')
+  end
+
+  it 'should return factorial of given number of 5' do
+    expect(@solver.factorial(5)).to eq 120
+  end
+end
